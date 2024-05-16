9e4e9f37ebf03c91892fbc34999fc2ebe3fa7e39
zhuweiyou <i@zhuweiyou.com>
2024-05-16 19:00:14 +0800

    监听单独处理而不是Set

--- src/main/java/com/zhuweiyou/msgbot/cron/bilibili/BilibiliCron.java
+++ src/main/java/com/zhuweiyou/msgbot/cron/bilibili/BilibiliCron.java
@@ -1,12 +1,10 @@
 package com.zhuweiyou.msgbot.cron.bilibili;
 
 import com.zhuweiyou.msgbot.platform.ntchat.NtchatPlatform;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 public class BilibiliCron {
     private final BilibiliConfig bilibiliConfig;
--- src/main/java/com/zhuweiyou/msgbot/cron/weibo/WeiboCron.java
+++ src/main/java/com/zhuweiyou/msgbot/cron/weibo/WeiboCron.java
@@ -1,12 +1,10 @@
 package com.zhuweiyou.msgbot.cron.weibo;
 
 import com.zhuweiyou.msgbot.platform.ntchat.NtchatPlatform;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 public class WeiboCron {
     private final WeiboConfig weiboConfig;
