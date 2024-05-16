2ebf402144bb4b0b853e2f7c8b9092554098c9c1
zhuweiyou <i@zhuweiyou.com>
2024-05-16 19:01:08 +0800

    单独处理监听 (#3)

--- src/main/java/com/zhuweiyou/msgbot/cron/weibo/WeiboCron.java
+++ src/main/java/com/zhuweiyou/msgbot/cron/weibo/WeiboCron.java
@@ -1,37 +1,30 @@
 package com.zhuweiyou.msgbot.cron.weibo;
 
-import java.util.Set;
-
+import com.zhuweiyou.msgbot.platform.ntchat.NtchatPlatform;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import com.zhuweiyou.msgbot.platform.ntchat.NtchatPlatform;
-
 @Component
 public class WeiboCron {
-	private final WeiboConfig weiboConfig;
-	private final Set<WeiboWatcher> weiboWatcherSet;
-	private final NtchatPlatform ntchatPlatform;
+    private final WeiboConfig weiboConfig;
+    private final WeiboWatcher ganggangWatcher;
+    private final NtchatPlatform ntchatPlatform;
 
-	@Autowired
-	public WeiboCron(WeiboConfig weiboConfig, GanggangWeiboFilter ganggangWeiboFilter, NtchatPlatform ntchatPlatform) {
-		this.weiboConfig = weiboConfig;
-		this.ntchatPlatform = ntchatPlatform;
-		this.weiboWatcherSet = Set.of(
-			new WeiboWatcher("5514146941", ganggangWeiboFilter)
-		);
-	}
+    @Autowired
+    public WeiboCron(WeiboConfig weiboConfig, GanggangWeiboFilter ganggangWeiboFilter, NtchatPlatform ntchatPlatform) {
+        this.weiboConfig = weiboConfig;
+        this.ntchatPlatform = ntchatPlatform;
+        this.ganggangWatcher = new WeiboWatcher("5514146941", ganggangWeiboFilter);
+    }
 
-	@Scheduled(cron = "0 */1 * * * *")
-	public void watchBlog() throws Exception {
-		for (WeiboWatcher weiboWatcher : weiboWatcherSet) {
-			weiboWatcher.getNewBlog().ifPresent(blog -> {
-				String msg = blog.toString();
-				for (String wxid : weiboConfig.getGanggangWxidList()) {
-					ntchatPlatform.sendPrivateText(wxid, msg);
-				}
-			});
-		}
-	}
+    @Scheduled(cron = "0 */1 * * * *")
+    public void watchGanggang() throws Exception {
+	    ganggangWatcher.getNewBlog().ifPresent(blog -> {
+            String msg = blog.toString();
+            for (String wxid : weiboConfig.getGanggangWxidList()) {
+                ntchatPlatform.sendPrivateText(wxid, msg);
+            }
+        });
+    }
 }
